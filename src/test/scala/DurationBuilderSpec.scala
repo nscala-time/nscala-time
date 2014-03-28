b86f2ab4347c646150b1e73c3692979d1dcd5aa4
xuwei-k <6b656e6a69@gmail.com>
2014-03-28 12:24:41 +0900

    add DurationBuilderSpec

---
+++ src/test/scala/DurationBuilderSpec.scala
@@ -0,0 +1,12 @@
+package com.github.nscala_time.time
+
+import com.github.nscala_time.time.Imports._
+
+object DurationBuilderSpec {
+
+  val a = 1.seconds + 2.seconds
+
+  val b = 5.seconds - 3.seconds
+
+}
+
