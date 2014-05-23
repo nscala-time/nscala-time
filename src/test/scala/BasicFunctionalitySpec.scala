7ac71c66c0d56424025cc97cd9b2a05091cdaa5f
Kota Mizushima <mizukota@gmail.com>
2014-05-23 22:02:39 +0900

    Add exmple using millis methodin fluent interface

--- src/test/scala/BasicFunctionalitySpec.scala
+++ src/test/scala/BasicFunctionalitySpec.scala
@@ -32,6 +32,18 @@ object BasicFunctionalitySpec extends Specification {
       val expected = DateTime.parse("2013-03-02T07:08:09.123+0900")
       actual must ===(expected)
     }
+    "have setters using millis" in {
+      val actual = DateTime.parse("2012-01-01T01:01:01.123+0900")
+        .year.apply(2013) // TODO cannot call #year(2013)
+        .month(3)
+        .day(2)
+        .hour(7)
+        .minute(8)
+        .second(9)
+        .millis(500)
+      val expected = DateTime.parse("2013-03-02T07:08:09.500+0900")
+      actual must ===(expected)
+    }
   }
 
 }
